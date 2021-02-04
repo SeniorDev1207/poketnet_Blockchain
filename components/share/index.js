13319a9ccf3193b5cb66475cf19470e1f0532f06
SeniorDev1207 <rabbitdev1207@gmail.com>
2021-02-04 17:43:03 +0300

    init attach poll

--- components/share/index.js
+++ components/share/index.js
@@ -126,10 +126,13 @@ var share = (function(){
 
 			autoFilled : function(){
 
+				console.log('current', currentShare);
+
 				actions.filled('i', currentShare.images.v.length != 0)
 				actions.filled('u', currentShare.url.v)
 				actions.filled('t',  currentShare.tags.v.length!= 0)					
 				actions.filled('cm', currentShare.message.v || currentShare.caption.v)
+				actions.filled('p', currentShare.poll.v.list)
 
 			},
 
@@ -252,12 +255,11 @@ var share = (function(){
 						id : 'streampeertube',
 						inWnd : true,
 
-						history : false,
+						history : true,
 
 						essenseData : {
 							storage : storage,
 							value : value,
-							currentLink : currentShare.url ? currentShare.url.v : '',
 							actions : {
 								added : function(link){
 									var type = 'url';
@@ -320,6 +322,8 @@ var share = (function(){
 
 					return
 				}
+
+				
 
 				if(type == 'times'){
 
@@ -400,6 +404,9 @@ var share = (function(){
 						}
 					})
 				}
+
+		
+
 
 				
 			},
@@ -525,6 +532,18 @@ var share = (function(){
 					state.save()
 				}
 			},
+
+			removePoll : function(){
+
+				currentShare.poll.set();
+
+				if(!essenseData.share){
+					state.save()
+				}
+				/*el.message.val(text);
+				el.message.change();*/
+			},
+
 			removelink : function(){
 
 				var l = currentShare.url.v
@@ -1039,9 +1058,51 @@ var share = (function(){
 			embeding : function(){
 				var type = $(this).attr('embeding')
 
+
 				if (type == 'language'){
 
 					actions.language()
+
+					return
+				}
+
+				if (type == 'poll'){
+
+					dialog({
+						header: "Create new poll",
+						class : "one joinbeta",
+						poll: true,
+						btn1text : 'Create',
+						success: function(){
+
+							var poll = $('.dialog .poll');
+							
+							var title = poll.find('.title .input').val();
+
+							var list = poll.find('.poll-item .input');
+
+							var values = list.map(function(idx, item){
+								return $(item).val();
+							})
+
+							.filter(function(idx, item){
+								return item;
+							})
+
+							values = Array.from(values);
+
+							var obj = {
+								title: title,
+								list: values
+							}
+
+							currentShare.poll.set(obj);
+
+							renders.poll()
+
+							console.log('create!!!', values, currentShare);
+						}
+					})
 
 					return
 				}
@@ -1141,6 +1202,12 @@ var share = (function(){
 				actions.removelink()
 
 				renders.url();
+			},
+
+			removePoll : function(){
+				actions.removePoll()
+
+				renders.poll();
 			}
 
 
@@ -1177,7 +1244,7 @@ var share = (function(){
 					el.peertubeLiveStream = el.c.find('.peertubeLiveStream');
 
 					el.peertube.on('click', async function() {
-						console.log('>>>>>>>>usertoken', self.app.peertubeHandler.userName, self.app.peertubeHandler.password);
+						console.log('>>>>>>>>usertoken', self.app.peertubeHandler.userToken);
 					});
 
 
@@ -1515,6 +1582,7 @@ var share = (function(){
 			},
 
 			images : function(clbk){
+
 				self.shell({
 					name :  'images',
 					turi : 'embeding',
@@ -1669,6 +1737,100 @@ var share = (function(){
 					
 				
 				})
+			},
+
+			poll : function(clbk){
+
+				var poll = currentShare.poll.get();
+
+				console.log('poll', poll, el);
+
+				var pollWrapper = p.el.find('.pollWrapper');
+
+				var content = '';
+				
+				var title = poll.title;
+
+				if (title){
+
+					content += `<div class="title"><b>${title}</b></div>`;
+
+				}
+
+
+				if (poll.list && poll.list.length){
+
+					var list= '<div class="list">';
+
+					poll.list.forEach(function(v){
+
+						list += `<div class="list-item">${v}</div>`
+					})
+
+					list += '</div>';
+
+					content += list;
+					
+				}
+
+				var removeWrapper = '<div class="removeWrapper"><div class="removelink"><i class="fas fa-times"></i></div></div>'
+
+
+				var html = '';
+
+				if (content){
+
+					html = '<div class="poll">' + content + removeWrapper + '</div>';
+				}
+
+
+				pollWrapper.html(html);
+
+				
+				p.el.find('.pollWrapper').on('click', function(){
+
+					events.removePoll();
+				})
+
+				
+				// self.shell({
+				// 	name :  'poll',
+				// 	inner : html,
+				// 	el : el.urlWrapper,
+				// 	data : {
+				// 		poll : poll,
+				// 		og : null,
+				// 		remove : true,
+
+				// 		share : currentShare
+				// 	},
+
+				// }, function(p){
+
+				// 	console.log('poll', poll, el)
+
+
+				// 	if(poll && !og){
+
+				// 		if (meta.type == 'youtube' || meta.type == 'vimeo' || meta.type == 'bitchute' || meta.type == 'peertube') {
+
+                //             Plyr.setup('.js-player', function(player) {
+
+				// 				player.muted = false
+				// 			});
+
+				// 		} else {
+				// 			self.app.platform.sdk.remote.get(meta.url, function(og){
+
+				// 				if(og){
+				// 					renders.url()
+				// 				}
+
+				// 			})
+				// 		}
+				// 	}
+
+				// })
 			}
 
 		}
@@ -1874,7 +2036,7 @@ var share = (function(){
 						na.push($(this).attr('part'))
 					})
 
-					currentShare.settings.a = na
+					console.log('na', na);
 
 					if(!essenseData.share){
 						state.save()
@@ -2023,6 +2185,8 @@ var share = (function(){
 				el.caption = el.c.find('.captionshare');
 				el.cpt = el.c.find('.cpt')
 				el.images = el.c.find('.imagesWrapper')
+
+				el.poll = el.c.find('.pollWrapper')
 
 				el.changeAddress = el.c.find('.changeAddress')
 
--- js/peertube-master.js
+++ js/peertube-master.js
@@ -8,19 +8,15 @@ PeerTubeHandler = function (app) {
       $.ajax({
         url: `${baseUrl}${method}`,
         ...parameters,
-      })
-        .done((res) => {
-          parameters.success(res);
-        })
-        .fail((res) => parameters.fail(res));
+      });
     },
 
     run({ method, parameters }) {
-      return fetch(`${baseUrl}${method}`, parameters)
-        .then((res) => res.json())
-        .catch((err) => {
-          return { error: err };
-        });
+      return fetch(`${baseUrl}${method}`, parameters).catch((err) => {
+        console.log(err);
+
+        return err;
+      });
     },
   };
 
@@ -222,10 +218,6 @@ PeerTubeHandler = function (app) {
 
           parameters.successFunction(`${watchUrl}${json.video.uuid}`);
         },
-
-        fail: () => {
-          return parameters.successFunction('error');
-        },
       },
     });
   };
@@ -242,94 +234,5 @@ PeerTubeHandler = function (app) {
         },
       },
     });
-  };
-
-  this.startLive = async (parameters) => {
-    const channelInfo = await this.getChannel();
-
-    const bodyOfQuery = {
-      privacy: 1,
-      'scheduleUpdate[updateAt]': new Date().toISOString(),
-      channelId: channelInfo.id,
-      name: parameters.name || `${this.userName}:${new Date().toISOString()}`,
-    };
-
-    if (parameters.image) {
-      bodyOfQuery.previewfile = parameters.image;
-      bodyOfQuery.thumbnailfile = parameters.image;
-    }
-
-    const formData = new FormData();
-
-    Object.keys(bodyOfQuery).map((key) =>
-      formData.append(key, bodyOfQuery[key]),
-    );
-
-    apiHandler.upload({
-      method: 'videos/live',
-      parameters: {
-        type: 'POST',
-        method: 'POST',
-        contentType: false,
-        processData: false,
-        data: formData,
-        headers: {
-          Authorization: `Bearer ${this.userToken}`,
-        },
-
-        // xhr: () => {
-        //   const xhr = $.ajaxSettings.xhr(); // получаем объект XMLHttpRequest
-        //   xhr.upload.addEventListener(
-        //     'progress',
-        //     function (evt) {
-        //       // добавляем обработчик события progress (onprogress)
-        //       if (evt.lengthComputable) {
-        //         const percentComplete = (evt.loaded / evt.total) * 100;
-
-        //         this.uploadProgress = percentComplete;
-        //         parameters.uploadFunction(percentComplete);
-        //       }
-        //     },
-        //     false,
-        //   );
-        //   return xhr;
-        // },
-
-        success: (json) => {
-          if (!json.video) return parameters.successFunction('error');
-
-          return this.getLiveInfo(json.video.uuid, {
-            successFunction: parameters.successFunction,
-          });
-        },
-
-        fail: (res) => {
-          return parameters.successFunction({ error: res });
-        },
-      },
-    });
-  };
-
-  this.getLiveInfo = async (id, parameters) => {
-    apiHandler
-      .run({
-        method: `videos/live/${id}`,
-        parameters: {
-          type: 'GET',
-          headers: {
-            Authorization: `Bearer ${this.userToken}`,
-          },
-        },
-      })
-      .then((res) => {
-        if (res.error) {
-          return parameters.successFunction(res);
-        }
-
-        return parameters.successFunction({
-          video: `${watchUrl}${id}`,
-          ...res,
-        });
-      });
   };
 };
