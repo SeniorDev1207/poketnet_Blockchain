b9261b7c56fac1d0a0603440b76b2518fb0362b5
SeniorDev1207 <rabbitdev1207@gmail.com>
2021-02-23 14:36:41 +0300

    peertube proxy init

---
+++ proxy16/peertube.js
@@ -0,0 +1,22 @@
+const Peertube = function () {
+  this.api = {
+    // mercatox: {
+    //   rate: function (currency) {
+    //   },
+    // },
+  };
+
+  this.destroy = () => {
+    return;
+  };
+
+  this.init = () => {
+    return;
+  };
+
+  this.kit = {};
+
+  return this;
+};
+
+module.exports = Peertube;
