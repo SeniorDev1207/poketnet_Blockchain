037960082388434d4df75e9b0fecdf93d5a44773
SeniorDev1207 <rabbitdev1207@gmail.com>
2021-03-04 17:42:24 +0300

    preloader and styles

--- components/share/index.js
+++ components/share/index.js
@@ -126,10 +126,13 @@ var share = (function(){
 
 			autoFilled : function(){
 
+				console.log('current', currentShare);
+
 				actions.filled('i', currentShare.images.v.length != 0)
 				actions.filled('u', currentShare.url.v)
 				actions.filled('t',  currentShare.tags.v.length!= 0)					
 				actions.filled('cm', currentShare.message.v || currentShare.caption.v)
+				actions.filled('p', currentShare.poll.v.list)
 
 			},
 
@@ -194,10 +197,15 @@ var share = (function(){
 				var storage = currentShare.export(true)
 
 				if (type === 'addVideo') {
+
+					globalpreloader(true);
+
 					el.peertube.addClass('disabledShare');
 					el.peertubeLiveStream.addClass('disabledShare');
 
 					self.app.peertubeHandler.authentificateUser(function(response) {
+						globalpreloader(false);
+
 						if (!response) response = {};
 
 						if (response.error) {
@@ -262,7 +270,10 @@ var share = (function(){
 					el.peertubeLiveStream.addClass('disabledShare');
 					el.peertube.addClass('disabledShare');
 
+					globalpreloader(true);
+
 					self.app.peertubeHandler.authentificateUser(function(response) {
+						globalpreloader(false);
 
 						if (!response) response = {};
 						
@@ -351,6 +362,8 @@ var share = (function(){
 					return
 				}
 
+				
+
 				if(type == 'times'){
 
 					dialog({
@@ -430,6 +443,9 @@ var share = (function(){
 						}
 					})
 				}
+
+		
+
 
 				
 			},
@@ -555,6 +571,18 @@ var share = (function(){
 					state.save()
 				}
 			},
+
+			removePoll : function(){
+
+				currentShare.poll.set();
+
+				if(!essenseData.share){
+					state.save()
+				}
+				/*el.message.val(text);
+				el.message.change();*/
+			},
+
 			removelink : function(){
 
 				var l = currentShare.url.v
@@ -1087,9 +1115,51 @@ var share = (function(){
 			embeding : function(){
 				var type = $(this).attr('embeding')
 
+
 				if (type == 'language'){
 
 					actions.language()
+
+					return
+				}
+
+				if (type == 'poll'){
+
+					dialog({
+						header: "Create new poll",
+						class : "one joinbeta",
+						poll: true,
+						btn1text : 'Create',
+						success: function(){
+
+							var poll = $('.dialog .poll');
+							
+							var title = poll.find('.title .input').val();
+
+							var list = poll.find('.poll-item .input');
+
+							var values = list.map(function(idx, item){
+								return $(item).val();
+							})
+
+							.filter(function(idx, item){
+								return item;
+							})
+
+							values = Array.from(values);
+
+							var obj = {
+								title: title,
+								list: values
+							}
+
+							currentShare.poll.set(obj);
+
+							renders.poll()
+
+							console.log('create!!!', values, currentShare);
+						}
+					})
 
 					return
 				}
@@ -1189,6 +1259,12 @@ var share = (function(){
 				actions.removelink()
 
 				renders.url();
+			},
+
+			removePoll : function(){
+				actions.removePoll()
+
+				renders.poll();
 			}
 
 
@@ -1459,6 +1535,8 @@ var share = (function(){
 
 				renders.images();
 
+				renders.poll();
+
 				renders.repost();
 
 				renders.postline();
@@ -1571,6 +1649,7 @@ var share = (function(){
 			},
 
 			images : function(clbk){
+
 				self.shell({
 					name :  'images',
 					turi : 'embeding',
@@ -1712,8 +1791,6 @@ var share = (function(){
 								});
 
 							})
-		
-							
 						}
 
 						if (clbk)
@@ -1725,6 +1802,98 @@ var share = (function(){
 					
 				
 				})
+			},
+
+			poll : function(clbk){
+
+				var poll = currentShare.poll.get();
+
+				console.log('poll', poll, el);
+
+				var pollWrapper = p.el.find('.pollWrapper');
+
+				var content = '';
+				
+				var title = poll.title;
+
+				if (title){
+
+					content += `<div class="title"><b>${title}</b></div>`;
+
+				}
+
+
+				if (poll.list && poll.list.length){
+
+					var list= '<div class="list">';
+
+					poll.list.forEach(function(v){
+
+						list += `<div class="list-item">${v}</div>`
+					})
+
+					list += '</div>';
+
+					content += list;
+					
+				}
+
+				var removeWrapper = '<div class="removeWrapper"><div class="removelink"><i class="fas fa-times"></i></div></div>'
+
+
+				var html = '';
+
+				if (content){
+
+					html = '<div class="poll">' + content + removeWrapper + '</div>';
+				}
+
+				pollWrapper.html(html);
+	
+				p.el.find('.pollWrapper').on('click', function(){
+
+					events.removePoll();
+				})
+
+				
+				// self.shell({
+				// 	name :  'poll',
+				// 	inner : html,
+				// 	el : el.urlWrapper,
+				// 	data : {
+				// 		poll : poll,
+				// 		og : null,
+				// 		remove : true,
+
+				// 		share : currentShare
+				// 	},
+
+				// }, function(p){
+
+				// 	console.log('poll', poll, el)
+
+
+				// 	if(poll && !og){
+
+				// 		if (meta.type == 'youtube' || meta.type == 'vimeo' || meta.type == 'bitchute' || meta.type == 'peertube') {
+
+                //             Plyr.setup('.js-player', function(player) {
+
+				// 				player.muted = false
+				// 			});
+
+				// 		} else {
+				// 			self.app.platform.sdk.remote.get(meta.url, function(og){
+
+				// 				if(og){
+				// 					renders.url()
+				// 				}
+
+				// 			})
+				// 		}
+				// 	}
+
+				// })
 			}
 
 		}
@@ -1930,7 +2099,7 @@ var share = (function(){
 						na.push($(this).attr('part'))
 					})
 
-					currentShare.settings.a = na
+					console.log('na', na);
 
 					if(!essenseData.share){
 						state.save()
@@ -1996,7 +2165,6 @@ var share = (function(){
 
 				intro = false;
 				external = null
-
 				currentShare = deep(p, 'settings.essenseData.share') || new Share(self.app.localization.key);
 				console.log('currentShare', currentShare)
 				essenseData = deep(p, 'settings.essenseData') || {};
@@ -2079,6 +2247,8 @@ var share = (function(){
 				el.caption = el.c.find('.captionshare');
 				el.cpt = el.c.find('.cpt')
 				el.images = el.c.find('.imagesWrapper')
+
+				el.poll = el.c.find('.pollWrapper')
 
 				el.changeAddress = el.c.find('.changeAddress')
 
--- main.js
+++ main.js
@@ -5,6 +5,8 @@ if (setupEvents.handleSquirrelEvent()) {
   return;
 }*/
 
+
+const {protocol} = require('electron');
 //const ProxyInterface = require('./proxy/mainserver.js')
 
 const ProxyInterface = require('./proxy16/ipc.js')
@@ -22,7 +24,8 @@ const Badge = require('./js/vendor/electron-windows-badge.js');
 // AutoUpdate --------------------------------------
 const { autoUpdater } = require("electron-updater");
 const log = require('electron-log');
-const is = require('electron-is')
+const is = require('electron-is');
+
 
 var updatesLoading = false;
 
@@ -270,6 +273,7 @@ function initApp() {
         win.webContents.send('resume-message', { msg: 'resume', type: 'info' })
 
     })
+
 }
 
 function closeNotification() {
@@ -400,19 +404,17 @@ function createWindow() {
               label: app.name,
               submenu: [
                 {
-                    accelerator: 'Cmd+A',
                     label: 'About',
                     click: async () => {
                         win.webContents.send('nav-message', { msg: 'about', type: 'action'})
                     }
                 },
                 { type: 'separator' },
-                { role: 'hide', accelerator: 'Cmd+W', },
+                { role: 'hide' },
                 { role: 'unhide' },
                 { type: 'separator' },
                 {
                     label: 'Quit Pocketnet',
-                    accelerator: 'Cmd+Q',
                     click: async () => {
                       quit()
                     }
@@ -480,7 +482,6 @@ function createWindow() {
               submenu: [
                 {
                   label: 'Help center',
-                  accelerator: 'Cmd+H',
                   click: async () => {
                     win.webContents.send('nav-message', { msg: 'help', type: 'action'})
                     }
@@ -505,6 +506,7 @@ function createWindow() {
 
     win.webContents.on('new-window', function(event, url) {
         event.preventDefault();
+        console.log('new-window', event, url);
         open(url);
     });
 
@@ -540,6 +542,12 @@ function createWindow() {
         callback({ cancel: false, responseHeaders: detail.responseHeaders });
     });
 
+
+    // console.log('process.argv', process.argv);
+    
+    // var href = process.argv[process.argv.length - 1].replace(/.+pocketnet\//, '');
+
+    // win.webContents.send('nav-message', { msg: href, type: 'action'})
 
 
     //
@@ -591,12 +599,37 @@ function createWindow() {
 }
 
 
+var openlink = function(argv, ini){
+
+    if (argv && argv.length && argv[argv.length - 1] && argv[argv.length - 1].indexOf('pocketnet://') > -1){
+
+        var href = argv[argv.length - 1].replace('pocketnet://electron/', '');
+
+        if (href && href[href.length - 1] == '/') href = href.substr(0, href.length - 1)
+
+        if(!href) href = 'index'
+
+        setTimeout(function(){
+
+            win.webContents.send('nav-message', { msg: href, type: 'action'})
+
+        }, ini ? 3000 : 5)
+
+    }
+}
+
 var r = app.requestSingleInstanceLock()
 
-if (!r) {
+if(!r) {
     app.quit()
 } else {
+
+    openlink(process.argv, true)
+    
     app.on('second-instance', function(event, argv, cwd) {
+
+        openlink(argv)
+
         if (win) {
 
             if (win.isMinimized()) win.restore();
@@ -606,7 +639,12 @@ if (!r) {
         }
     })
 
-    app.setAsDefaultProtocolClient('pocketnet')
+    // If we are running a non-packaged version of the app && on windows
+
+
+    app.setAsDefaultProtocolClient('pocketnet', process.execPath, [path.resolve(process.argv[1])]);  
+    
+
 
     // Этот метод будет вызываться, когда Electron закончит 
     // инициализацию и готов к созданию окон браузера.
@@ -621,6 +659,7 @@ if (!r) {
             app.quit()
         }
     })
+
 
     app.on('activate', () => {
         // На MacOS обычно пересоздают окно в приложении,
