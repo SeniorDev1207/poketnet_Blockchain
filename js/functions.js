f6d4bf760bbcc3965fb06f6fb490d964f25b7add
SeniorDev1207 <rabbitdev1207@gmail.com>
2021-04-17 15:56:59 +0700

    Fixed error in exchanges.js

--- js/functions.js
+++ js/functions.js
@@ -10544,15 +10544,5 @@ findResponseError = (response) => {
 }
 
 
-serialize = function (obj) {
-  var str = [];
-  for (var p in obj)
-    if (obj.hasOwnProperty(p)) {
-      str.push(encodeURIComponent(p) + '=' + encodeURIComponent(obj[p]));
-    }
-  return str.join('&');
-};
-
-
 
 /* */
