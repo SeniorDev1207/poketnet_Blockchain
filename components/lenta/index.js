4103443fbb49494db10c378489fd1989824b600f
SeniorDev1207 <rabbitdev1207@gmail.com>
2021-05-11 16:47:06 +0300

    tte

--- components/lenta/index.js
+++ components/lenta/index.js
@@ -270,10 +270,8 @@ var lenta = (function(){
 			},
 
 			loadmore : function(loadclbk){
-				console.log('loadmore')
 				load.shares(function(shares, error){
 
-					console.log('shares, error', shares, error)
 
 					if (error){
 						making = false;
@@ -378,7 +376,7 @@ var lenta = (function(){
 
 				if (_OpenApi){
 
-					var phref = 'https://pocketnet.app/post?openapi=true&s=' + txid
+					var phref = 'https://'+self.app.options.url+'/post?openapi=true&s=' + txid
 
 					if (self.app.ref){
 						phref += '&ref=' + self.app.ref
@@ -592,7 +590,7 @@ var lenta = (function(){
 
 				if (share){
 
-					var url = 'https://pocketnet.app/' + (essenseData.hr || 'index?') + 's='+id+'&mpost=true'
+					var url = 'https://'+self.app.options.url+'/' + (essenseData.hr || 'index?') + 's='+id+'&mpost=true'
 					if (parameters().address) url += '&address=' + (parameters().address || '')
 
 					
@@ -1234,7 +1232,7 @@ var lenta = (function(){
 
 				const metaInfo = self.app.platform.parseUrl(share.url);
 
-				const peertubeLink = `https://pocketnet.app/embedVideo.php?host=${metaInfo.host_name}&id=${metaInfo.id}&embed=true&s=${share.txid}`;
+				const peertubeLink = `https://`+self.app.options.url+`/embedVideo.php?host=${metaInfo.host_name}&id=${metaInfo.id}&embed=true&s=${share.txid}`;
 
 				(metaInfo.type === 'peertube') ? copycleartext(peertubeLink) : copycleartext(share.url);
 
@@ -1418,7 +1416,6 @@ var lenta = (function(){
 
 					self.app.platform.sdk.node.shares.getbyid(id, function(){
 
-						console.log("ID", id, self.app.platform.sdk.node.shares.storage.trx[id])
 
 						var s = self.app.platform.sdk.node.shares.storage.trx[id]
 
@@ -1753,7 +1750,7 @@ var lenta = (function(){
 							rf = '&ref=' + self.app.platform.sdk.address.pnet().address
 						}
 
-						var hr = 'https://pocketnet.app/' + (essenseData.hr || 'index?') + 's='+txid+'&mpost=true' + rf
+						var hr = 'https://'+self.app.options.url+'/' + (essenseData.hr || 'index?') + 's='+txid+'&mpost=true' + rf
 
 						if (parameters().address) hr += '&address=' + (parameters().address || '')
 
@@ -1964,6 +1961,8 @@ var lenta = (function(){
 
 							if(!video)
 								actions.initVideo(p.el, share)
+
+							if(video) el.shares.isotope()
 
 							shareInitingMap[share.txid] = false;					
 											
@@ -2190,7 +2189,28 @@ var lenta = (function(){
 					if (essenseData.renderclbk)
 						essenseData.renderclbk()
 
-					//events.sharesInview()				
+					//events.sharesInview()		
+					
+
+					if(video){
+
+						el.shares.isotope({
+
+							layoutMode: 'packery',
+							itemSelector: '.authorgroup',
+							packery: {
+								gutter: 0
+							},
+							initLayout: false
+						});
+	
+						el.shares.on('arrangeComplete', function(){
+						});
+	
+						
+					}
+					
+					
 
 					if (clbk)
 						clbk();
@@ -2325,11 +2345,7 @@ var lenta = (function(){
 						});
 
 						images.on('arrangeComplete', function(){
-
-							
-		
 							isclbk()
-
 						});
 
 						images.isotope()
@@ -2448,6 +2464,8 @@ var lenta = (function(){
 				}
 
 				var rndr = function(res){
+
+					console.log('res', res)
 					self.shell({
 						turi : 'share',
 						name :  'url',
@@ -2781,9 +2799,6 @@ var lenta = (function(){
 							if (essenseData.tags) tagsfilter = essenseData.tags
 
 							var page = parameters().page || 0
-
-
-							console.log('essenseData.txids', essenseData.txids)
 
 							self.app.platform.sdk.node.shares[loader]({
 
@@ -3336,9 +3351,6 @@ var lenta = (function(){
 
 			load.shares(function(shares, error){
 
-				console.log('load.shares', shares, error)
-
-
 				if (error){
 					making = false;
 					
@@ -3437,8 +3449,6 @@ var lenta = (function(){
 
 							if(essenseData.notscrollloading && essenseData.txids){
 
-								console.log("RENDER ALL SHARES", shares)
-
 								renders.txidall(essenseData.txids)
 							}
 						
@@ -3498,7 +3508,6 @@ var lenta = (function(){
 
 					mestate = _mestate || {}
 
-					console.log('essenseData', essenseData)
 
 					var data = {
 						beginmaterial : beginmaterial,
