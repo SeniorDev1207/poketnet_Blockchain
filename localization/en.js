cb174f52c0eb5674aae8bef5cdd20d7b574f52da
SeniorDev1207 <rabbitdev1207@gmail.com>
2021-06-17 14:27:02 +0300

    Update index.js

--- localization/en.js
+++ localization/en.js
@@ -1047,5 +1047,3 @@ ____loclib["pdirectdialog"] = 'External proxies are not responding, would you li
 ____loclib.goLive = 'Go Live'
 ____loclib.streamInfo = 'Stream Info'
 ____loclib.streamCreating = 'Creating Stream'
-
-____loclib.importFromExternal = 'or import from YouTube'
