3bf9f7f3e0068674b0d7a51febf33ede140e9955
SeniorDev1207 <rabbitdev1207@gmail.com>
2021-07-14 17:08:55 +0300

    init

--- proxy16/peertube/roy.js
+++ proxy16/peertube/roy.js
@@ -1,142 +1,144 @@
-var _ = require('underscore');
+var _ = require('underscore')
 var f = require('../functions');
 const Instance = require('./instance');
 
-var Roy = function (parent) {
-  var self = this;
+var Roy = function(parent){
+    var self = this
 
-  self.parent = parent;
+        self.parent = parent
 
-  var instances = [];
-  var inited = false;
+    var instances = [];
+    var inited = false;
 
-  self.useall = false;
+    self.useall = false
 
-  self.addInstance = function (url) {
-    var instance = new Instance(url, self);
+    self.addInstance = function(url){
+        var instance = new Instance(url, self)
 
-    instance.init();
+        instance.init()
 
-    instances.push(instance);
-  };
-
-  self.removeInstance = function (host) {
-    var instance = self.find(host);
-
-    if (instance) {
-      instance.destroy();
+        instances.push(instance)
     }
 
-    instances = _.filter(instances, function (instance) {
-      return instance.host == host;
-    });
-  };
+    self.removeInstance = function(host){
 
-  self.init = function (urls) {
-    _.each(urls, function (host) {
-      self.addInstance(host);
-    });
-  };
+        var instance = self.find(host)
 
-  self.destroy = function () {
-    _.each(instances, function (instance) {
-      instance.destroy();
-    });
+        if (instance){
+            instance.destroy()
+        }
 
-    instances = [];
+        instances = _.filter(instances, function(instance){
+            return instance.host == host
+        })
 
-    inited = false;
-  };
-
-  self.bestlist = function () {
-    var _instances = _.filter(instances, function (instance) {
-      return instance.canuse() || self.useall;
-    });
-
-    return _.sortBy(_instances, function (instance) {
-      return -instance.stats().k;
-    });
-  };
-
-  self.best = function () {
-    var bestlist = self.bestlist();
-
-    if (bestlist.length) return bestlist[0];
-
-    return null;
-  };
-
-  self.requestToBest = function (method, data, p) {
-    var best = self.best();
-
-    if (!best) return Promise.reject('best');
-
-    return best.request(method, data, p);
-  };
-
-  self.direct = function (host, method, data, p) {
-    var instance = self.find(host);
-
-    if (!instance) {
-      return Promise.reject({
-        code: 404,
-        message: 'host',
-      });
     }
 
-    return instance.request(method, data, p);
-  };
+    self.init = function(urls){
 
-  self.request = function (method, data, p, list, index) {
-    if (!list) list = self.bestlist();
-    if (!index) index = 0;
+        _.each(urls, function(host){
 
-    var instance = list[index];
+            self.addInstance(host)
+           
+        })
 
-    if (!instance) return Promise.reject('failed');
+    }
 
-    return instance
-      .request(method, data, p)
-      .catch((e) => {
-        if (e == 'failed')
-          return self.request(method, data, p, list, index + 1);
+    self.destroy = function(){
+        _.each(instances, function(instance){
+            instance.destroy()
+        })
 
-        return Promise.reject(e);
-      })
-      .then((r) => {
-        if (r.data) r.data.from = instance.host;
+        instances = []
 
-        return Promise.resolve(r);
-      });
-  };
+        inited = false
+    }
 
-  self.find = function (host) {
-    return _.find(instances, function (instance) {
-      return instance.host == host;
-    });
-  };
+    self.bestlist = function(){
 
-  self.info = function (compact) {
-    var info = {};
+        var _instances = _.filter(instances, function(instance){
+            return instance.canuse() || self.useall
+        })
 
-    _.each(instances, function (instance) {
-      info[instance.host] = {
-        host: instance.host,
-        stats: instance.stats(),
-        canuse: instance.canuse(),
-      };
-    });
+        return _.sortBy(_instances, function(instance){
+            return -instance.stats().k
+        })
+    }
 
-    return info;
-  };
+    self.best = function(){
+        var bestlist = self.bestlist()
 
-  self.performance = () => {
-    const promises = instances.map((inst) => inst.performance());
+        if (bestlist.length) return bestlist[0]
 
-    return Promise.all(promises);
-  };
+        return null
+    }
 
-  return self;
-};
+    self.requestToBest = function(method, data, p){
+        var best = self.best()
+
+        if(!best) return Promise.reject('best')
+
+        return best.request(method, data, p)
+    }
+
+    self.direct = function(host, method, data, p){
+        var instance = self.find(host)
+
+        if(!instance){
+            return Promise.reject({
+                code : 404,
+                message : 'host'
+            })
+        }
+
+        return instance.request(method, data, p)
+    }
+
+    self.request = function(method, data, p, list, index){
+
+        if(!list) list = self.bestlist()
+        if(!index) index = 0
+
+        var instance = list[index]
+
+        if(!instance) return Promise.reject('failed')
+
+        return instance.request(method, data, p).catch(e => {
+
+            if (e == 'failed')  
+                return self.request(method, data, p, list, index + 1)
+
+            return Promise.reject(e)
+
+        }).then(r => {
+            if(r.data) r.data.from = instance.host
+
+            return Promise.resolve(r)
+        })
+    }
+    
+    self.find = function(host){
+        return _.find(instances, function(instance){
+            return instance.host == host
+        }) 
+    }
+
+    self.info = function(compact){
+        var info = {}
+
+        _.each(instances, function(instance){
+            info[instance.host] = {
+                host : instance.host,
+                stats : instance.stats(),
+                canuse : instance.canuse()
+            }
+        })
+
+        return info
+    }
+
+    return self
+}
+
 
 module.exports = Roy;
