4c01f918e3d28f73efc79d2843efb345a32e885a
SeniorDev1207 <rabbitdev1207@gmail.com>
2021-07-21 10:07:56 +0300

    f

--- proxy16/lib/btc16.js
+++ proxy16/lib/btc16.js
@@ -36482,14 +36482,14 @@
     },
     testnet: {
       messagePrefix: '\x18Bitcoin Signed Message:\n',
-      bech32: 'bc',
+      bech32: 'tb',
       bip32: {
         public: 0x043587cf,
         private: 0x04358394
       },
-      pubKeyHash: 0x41,
-      scriptHash: 0x4e,
-      wif: 0x1e,
+      pubKeyHash: 0x6f,
+      scriptHash: 0xc4,
+      wif: 0xef
     }
   }
   
