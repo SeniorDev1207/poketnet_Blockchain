04230010e51e2842b47e1f190b171975fb9b9f30
SeniorDev1207 <rabbitdev1207@gmail.com>
2021-07-23 14:08:39 +0300

    added html template

--- proxy16/pocketnet.js
+++ proxy16/pocketnet.js
@@ -1,8 +1,7 @@
 var bitcoin = require('./lib/btc16.js');
 var pkit = require('./lib/kit.js');
 
-
-console.log('bitcoin', bitcoin.networks);
+console.log('bitcoin',  bitcoin.networks)
 
 var Pocketnet = function(){
     var self = this
